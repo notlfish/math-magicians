951374862337f5d8453ce654e05437138aae1de8
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-09-21 15:21:31 -0300

    Add Home and about pages, and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
-import Calculator from './components/Calculator';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Calculator from './pages/CalculatorPage';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Header from './components/Header';
 import './App.css';
 
-const App = () => <Calculator />;
+const App = () => (
+  <div>
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/quote">
+            <Quote />
+          </Route>
+          <Route path="/calculator">
+            <Calculator />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home-container">
+    <h2>Welcome to our page!</h2>
+    <p>
+      <p>
+        ’He’s dreaming now,’ said Tweedledee: ‘and what do you think he’s
+        dreaming about?’
+      </p>
+
+      <p>
+        Alice said ‘Nobody can guess that.’
+      </p>
+
+      <p>
+        ‘Why, about you!’ Tweedledee exclaimed, clapping his hands triumphantly.
+        ‘And if he left off dreaming about you, where do you suppose you’d be?’
+      </p>
+
+      <p>
+        ‘Where I am now, of course,’ said Alice.
+      </p>
+
+      <p>
+        ‘Not you!’ Tweedledee retorted contemptuously. ‘You’d be nowhere.
+        Why, you’re only a sort of thing in his dream!’
+      </p>
+
+      <p>
+        ‘If that there King was to wake,’ added Tweedledum,
+        ‘you’d go out—bang!—just like a candle!’
+      </p>
+
+      <p>
+        ‘I shouldn’t!’ Alice exclaimed indignantly. ‘Besides, if I’m only
+        a sort of thing in his dream, what are you, I should like to know?’
+      </p>
+
+      <p>
+        ‘Ditto’ said Tweedledum.
+      </p>
+
+      <p>
+        ‘Ditto, ditto’ cried Tweedledee.
+      </p>
+
+      <p>
+        He shouted this so loud that Alice couldn’t help saying,
+        ‘Hush! You’ll be waking him, I’m afraid, if you make so much noise.’
+      </p>
+
+      <p>
+        ‘Well, it no use your talking about waking him,’ said Tweedledum,
+        ‘when you’re only one of the things in his dream. You know very well
+        you’re not real.’
+      </p>
+
+      <p>
+        ‘I am real!’ said Alice and began to cry.
+      </p>
+
+      <p>
+        ‘You won’t make yourself a bit realler by crying,’ Tweedledee remarked:
+        ‘there’s nothing to cry about.’
+      </p>
+    </p>
+  </div>
+);
+export default Home;
